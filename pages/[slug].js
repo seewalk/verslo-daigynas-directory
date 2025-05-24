fb0296341fce65dc06475dfe35398ca2cc2d1ced
seewalk <negotiables2024@gmail.com>
2025-05-24 19:37:45 +0100

    Update [slug].js

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,1 +1,11 @@
 // TODO: Add dynamic vendor page code here
+import React from 'react';
+
+
+export default function VendorPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">Vendor page coming soon...</h1>
+    </div>
+  );
+}
