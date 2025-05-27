d7178dbeb4071e8f5c68024e5ba65aedba781424
Paula Žemaitė <paulavicius.e@gmail.com>
2025-05-28 00:20:54 +0100

    CSS styles update

--- pages/dashboard/claim-business.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import '../styles/globals.css';
-import { appWithTranslation } from 'next-i18next';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default appWithTranslation(MyApp);
